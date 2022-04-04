af31c3d23dc903d70f6d491c0d66a9466ad6e41d
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-04-04 21:01:39 +0200

    � NEW: Add getUserById route

---
+++ src/Controller/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+
+/**
+ * @Route("api")
+ */
+class UserController extends AbstractFOSRestController
+{
+    /**
+     * @Get(
+     *     path = "/user/{id}",
+     *     name = "app_article_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @View
+     */
+    public function getUserId(User $user)
+    {
+        return $user;
+    }
+}
