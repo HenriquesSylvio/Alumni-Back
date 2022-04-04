373d78127180ee1d88177eac5e6d68090e13f2a9
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-04-04 22:18:32 +0200

    📦 NEW: Add getMyProfile route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,16 +10,27 @@ use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Security\Core\Security;
 
 /**
- * @Route("api")
+ * @Route("api/user")
  */
 class UserController extends AbstractFOSRestController
 {
     /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
      * @Get(
-     *     path = "/user/{id}",
-     *     name = "app_article_show",
+     *     path = "/{id}",
+     *     name = "user_show",
      *     requirements = {"id"="\d+"}
      * )
      * @Rest\View(serializerGroups={"getUser"})
@@ -27,5 +38,17 @@ class UserController extends AbstractFOSRestController
     public function getUserById(User $user)
     {
         return $user;
+    }
+
+    /**
+     * @Get(
+     *     path = "/me",
+     *     name = "me_show",
+     * )
+     * @Rest\View(serializerGroups={"getUser"})
+     */
+    public function getMyProfile()
+    {
+        return $this->security->getUser();
     }
 }
