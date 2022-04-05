0f080f1345543923cf309bb2fdf2bf805d970e74
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-04-05 19:08:22 +0200

    👌 IMPROVE: The connected user is automatically the author of the post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Validator\ConstraintViolationList;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
@@ -18,6 +19,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
  */
 class PostController extends AbstractFOSRestController
 {
+
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     /**
      * @Rest\Post(
      *     name = "add_post"
@@ -27,6 +39,8 @@ class PostController extends AbstractFOSRestController
      */
     public function addPost(Post $post, ManagerRegistry $doctrine, ConstraintViolationList $violations)
     {
+        $post->setAuthor($this->security->getUser());
+        
         if (count($violations)) {
             foreach($violations as $error)
             {
