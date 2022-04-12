6d7908f0bcb42dfbf1e390855f1bdad993358948
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-04-12 23:29:31 +0200

    📦 NEW: Add acceptUser route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Patch;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Security\Core\Security;
+use FOS\RestBundle\Controller\Annotations\Get;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("api/user")
@@ -22,9 +25,12 @@ class UserController extends AbstractFOSRestController
      */
     private $security;
 
-    public function __construct(Security $security)
+    private $doctrine;
+
+    public function __construct(Security $security, ManagerRegistry $doctrine)
     {
         $this->security = $security;
+        $this->doctrine = $doctrine;
     }
 
     /**
@@ -38,6 +44,26 @@ class UserController extends AbstractFOSRestController
     public function getUserById(User $user)
     {
         return $user;
+    }
+
+    /**
+     * @Rest\Patch(
+     *     path = "/{id}",
+     *     name = "accept_user",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(StatusCode = 204)
+     */
+    public function acceptUser(User $user)
+    {
+        $user->setAcceptAccount(true);
+
+        $em = $this->doctrine->getManager();
+
+        $em->persist($user);
+        $em->flush();
+
+        return ;
     }
 
     /**
