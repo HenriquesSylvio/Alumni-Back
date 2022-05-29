ef4cc8d4578e20e91bd4220d7a9b552e4f1e25ec
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-05-29 15:49:28 +0200

    📦 NEW: Add Message Entity

---
+++ src/Entity/Message.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="messages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $sentBy;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="messages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $receivedBy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeInterface $createAt): self
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getSentBy(): ?User
+    {
+        return $this->sentBy;
+    }
+
+    public function setSentBy(?User $sentBy): self
+    {
+        $this->sentBy = $sentBy;
+
+        return $this;
+    }
+
+    public function getReceivedBy(): ?User
+    {
+        return $this->receivedBy;
+    }
+
+    public function setReceivedBy(?User $receivedBy): self
+    {
+        $this->receivedBy = $receivedBy;
+
+        return $this;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -132,6 +132,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $subscribes;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Message::class, mappedBy="sentBy", orphanRemoval=true)
+     */
+    private $messages;
+
     public function __construct()
     {
         $this->posts = new ArrayCollection();
@@ -140,6 +145,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->events = new ArrayCollection();
         $this->participates = new ArrayCollection();
         $this->subscribes = new ArrayCollection();
+        $this->messages = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -451,6 +457,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             // set the owning side to null (unless already changed)
             if ($subscribe->getSubscription() === $this) {
                 $subscribe->setSubscription(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setSentBy($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getSentBy() === $this) {
+                $message->setSentBy(null);
             }
         }
 
