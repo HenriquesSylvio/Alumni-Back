e15dc12c142319e2fcdf2ea2c0c675cddd383b56
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2022-06-08 22:39:39 +0200

    🐛 FIX: Try to fix cicd

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -74,125 +74,125 @@ class AppFixtures extends Fixture
         $user->setPromo(new \DateTime(2021-9-01));
         $user->setAcceptAccount(true);
         $manager->persist($user);
-//
-//        $postPrincipal = new Post();
-//        $postPrincipal->setContent("Ceci est un test");
-//        $postPrincipal->setCreateAt(new \DateTime(2022-4-21));
-//        $postPrincipal->setAuthor($user);
-//        $postPrincipal->setTag($tag);
-//        $manager->persist($postPrincipal);
-//
-//        for($nbPosts = 1; $nbPosts <= 30; $nbPosts++){
-//            $post = new Post();
-//            $post->setContent("Ceci est un test");
-//            $post->setCreateAt(new \DateTime(2022-4-21));
-//            $post->setAuthor($user);
-//            $post->setTag($tag);
-//            $manager->persist($post);
-//        }
-//
-//        for($nbComments = 1; $nbComments <= 30; $nbComments++){
-//            $comment = new Comment();
-//            $comment->setContent("Ceci est un test");
-//            $comment->setCreateAt(new \DateTime(2022-4-21));
-//            $comment->setAuthor($user);
-//            $comment->setPost($post);
-//            $manager->persist($comment);
-//        }
-//
-//        for($nbEvent = 1; $nbEvent <= 30; $nbEvent++){
-//            $event = new Event();
-//            $event->setTitle("Ceci est un test");
-//            $event->setDescription("Ceci est un test");
-//            $event->setDate(new \DateTime(date("d-m-Y")));
-//            $event->setAuthor($user);
-//            $manager->persist($event);
-//        }
-//
-//        $user = new User();
-//        $user->setEmail("user@outlook.fr");
-//        $user->setUsername('user');
-//        $user->setRoles(['ROLE_USER']);
-//        $user->setPassword($this->passwordHasher->hashPassword($user, '54875487'));
-//        $user->setLastName("Henriques");
-//        $user->setFirstname("Sylvio");
-//        $user->setBirthday(new \DateTime(1999-9-25));
-//        $user->setPromo(new \DateTime(2021-9-01));
-//        $user->setAcceptAccount(true);
-//        $manager->persist($user);
-//
-//
-//        for($nbPosts = 1; $nbPosts <= 30; $nbPosts++){
-//            $post = new Post();
-//            $post->setContent("Ceci est un test");
-//            $post->setCreateAt(new \DateTime(2022-4-21));
-//            $post->setAuthor($user);
-//            $post->setTag($tag);
-//            $manager->persist($post);
-//        }
-//
-//        for($nbComments = 1; $nbComments <= 30; $nbComments++){
-//            $comment = new Comment();
-//            $comment->setContent("Ceci est un test");
-//            $comment->setCreateAt(new \DateTime(2022-4-21));
-//            $comment->setAuthor($user);
-//            $comment->setPost($post);
-//            $manager->persist($comment);
-//        }
-//
-//        for($nbEvent = 1; $nbEvent <= 30; $nbEvent++){
-//            $event = new Event();
-//            $event->setTitle("Ceci est un test");
-//            $event->setDescription("Ceci est un test");
-//            $event->setDate(new \DateTime(2022-4-21));
-//            $event->setAuthor($user);
-//            $manager->persist($event);
-//        }
-//
-//        $user->setAcceptAccount(true);
-//
-//        $faker = Faker\Factory::create('fr_FR');
-//        for($nbUsers = 1; $nbUsers <= 30; $nbUsers++){
-//            $user = new User();
-//            $user->setEmail($faker->email);
-//            $user->setUsername($faker->userName);
-//            if($nbUsers === 1)
-//            {
-//                $user->setRoles(['ROLE_ADMIN']);
-//                $user->setAcceptAccount(true);
-//            } else {
-//                $user->setRoles(['ROLE_USER']);
-//                $user->setAcceptAccount(false);
-//            }
-//            $user->setPassword($this->passwordHasher->hashPassword($user, '54875487'));
-//            $user->setLastName($faker->lastName);
-//            $user->setFirstname($faker->firstName);
-//            $user->setBirthday(new \DateTime(1999-9-25));
-//            $user->setPromo(new \DateTime(2021-9-01));
-//            $manager->persist($user);
-//            $post = new Post();
-//            $post->setContent("Ceci est un test");
-//            $post->setCreateAt(new \DateTime(2022-4-21));
-//            $post->setAuthor($user);
-//            $post->setTag($tag);
-//            $manager->persist($post);
-//            $likePost = new LikePost();
-//            $likePost->setUsers($user);
-//            $likePost->setPost($postPrincipal);
-//            $manager->persist($likePost);
-//            $comment = new Comment();
-//            $comment->setContent("Ceci est un test");
-//            $comment->setCreateAt(new \DateTime(2022-4-21));
-//            $comment->setAuthor($user);
-//            $comment->setPost($postPrincipal);
-//            $manager->persist($comment);
-//            $event = new Event();
-//            $event->setTitle("Ceci est un test");
-//            $event->setDescription("Ceci est un test");
-//            $event->setDate(new \DateTime(2022-4-21));
-//            $event->setAuthor($user);
-//            $manager->persist($event);
-//        }
+
+        $postPrincipal = new Post();
+        $postPrincipal->setContent("Ceci est un test");
+        $postPrincipal->setCreateAt(new \DateTime(2022-4-21));
+        $postPrincipal->setAuthor($user);
+        $postPrincipal->setTag($tag);
+        $manager->persist($postPrincipal);
+
+        for($nbPosts = 1; $nbPosts <= 30; $nbPosts++){
+            $post = new Post();
+            $post->setContent("Ceci est un test");
+            $post->setCreateAt(new \DateTime(2022-4-21));
+            $post->setAuthor($user);
+            $post->setTag($tag);
+            $manager->persist($post);
+        }
+
+        for($nbComments = 1; $nbComments <= 30; $nbComments++){
+            $comment = new Comment();
+            $comment->setContent("Ceci est un test");
+            $comment->setCreateAt(new \DateTime(2022-4-21));
+            $comment->setAuthor($user);
+            $comment->setPost($post);
+            $manager->persist($comment);
+        }
+
+        for($nbEvent = 1; $nbEvent <= 30; $nbEvent++){
+            $event = new Event();
+            $event->setTitle("Ceci est un test");
+            $event->setDescription("Ceci est un test");
+            $event->setDate(new \DateTime(date("d-m-Y")));
+            $event->setAuthor($user);
+            $manager->persist($event);
+        }
+
+        $user = new User();
+        $user->setEmail("user@outlook.fr");
+        $user->setUsername('user');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, '54875487'));
+        $user->setLastName("Henriques");
+        $user->setFirstname("Sylvio");
+        $user->setBirthday(new \DateTime(1999-9-25));
+        $user->setPromo(new \DateTime(2021-9-01));
+        $user->setAcceptAccount(true);
+        $manager->persist($user);
+
+
+        for($nbPosts = 1; $nbPosts <= 30; $nbPosts++){
+            $post = new Post();
+            $post->setContent("Ceci est un test");
+            $post->setCreateAt(new \DateTime(2022-4-21));
+            $post->setAuthor($user);
+            $post->setTag($tag);
+            $manager->persist($post);
+        }
+
+        for($nbComments = 1; $nbComments <= 30; $nbComments++){
+            $comment = new Comment();
+            $comment->setContent("Ceci est un test");
+            $comment->setCreateAt(new \DateTime(2022-4-21));
+            $comment->setAuthor($user);
+            $comment->setPost($post);
+            $manager->persist($comment);
+        }
+
+        for($nbEvent = 1; $nbEvent <= 30; $nbEvent++){
+            $event = new Event();
+            $event->setTitle("Ceci est un test");
+            $event->setDescription("Ceci est un test");
+            $event->setDate(new \DateTime(2022-4-21));
+            $event->setAuthor($user);
+            $manager->persist($event);
+        }
+
+        $user->setAcceptAccount(true);
+
+        $faker = Faker\Factory::create('fr_FR');
+        for($nbUsers = 1; $nbUsers <= 30; $nbUsers++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setUsername($faker->userName);
+            if($nbUsers === 1)
+            {
+                $user->setRoles(['ROLE_ADMIN']);
+                $user->setAcceptAccount(true);
+            } else {
+                $user->setRoles(['ROLE_USER']);
+                $user->setAcceptAccount(false);
+            }
+            $user->setPassword($this->passwordHasher->hashPassword($user, '54875487'));
+            $user->setLastName($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setBirthday(new \DateTime(1999-9-25));
+            $user->setPromo(new \DateTime(2021-9-01));
+            $manager->persist($user);
+            $post = new Post();
+            $post->setContent("Ceci est un test");
+            $post->setCreateAt(new \DateTime(2022-4-21));
+            $post->setAuthor($user);
+            $post->setTag($tag);
+            $manager->persist($post);
+            $likePost = new LikePost();
+            $likePost->setUsers($user);
+            $likePost->setPost($postPrincipal);
+            $manager->persist($likePost);
+            $comment = new Comment();
+            $comment->setContent("Ceci est un test");
+            $comment->setCreateAt(new \DateTime(2022-4-21));
+            $comment->setAuthor($user);
+            $comment->setPost($postPrincipal);
+            $manager->persist($comment);
+            $event = new Event();
+            $event->setTitle("Ceci est un test");
+            $event->setDescription("Ceci est un test");
+            $event->setDate(new \DateTime(2022-4-21));
+            $event->setAuthor($user);
+            $manager->persist($event);
+        }
 
         $manager->flush();
     }
